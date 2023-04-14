4de14f3189ab59326f804ae15fa6d3e296974c34
Patryk Kozłowski <patryk.kozlowski@wercom.pl>
2023-04-14 14:01:59 +0200

    Fully working now

--- FunctionEngine.cs
+++ FunctionEngine.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SzyfrBlokowyV5._3
+﻿namespace SzyfrBlokowyV5._3
 {
     public class FunctionEngine
     {
@@ -22,18 +16,21 @@ namespace SzyfrBlokowyV5._3
             {
                 bool[] func = FunctionS(arrRight, keyItems[round]);
                 bool[] xor = ArraysXor(arrLeft, func);
-                var switchArr = SwitchArrays(xor, arrRight);
-                arrLeft = switchArr.Item1;
-                arrRight = switchArr.Item2;
+
+                if (round < 7)
+                {
+                    var switchArr = SwitchArrays(xor, arrRight);
+                    arrLeft = switchArr.Item1;
+                    arrRight = switchArr.Item2;
+                }
+                else { arrLeft= xor; }
                 round++;
-                //main algorythm x 8 rounds
             }
             bool[] result = KeyGeneratorEngine.Merge(arrLeft, arrRight);
             return result;
         }
         public static string Encrypt(bool[] arrLeft, bool[] arrRight, bool[][] keyItems)
         {
-            //encrypting algorythm
             bool[] result = RoundEngine(arrLeft, arrRight, keyItems, 0);
             return ConvertExtension.ArrayToString(result);
         }
@@ -41,16 +38,10 @@ namespace SzyfrBlokowyV5._3
         {
             bool[] result = RoundEngine(arrLeft, arrRight, keyItems, 1);
             return ConvertExtension.ArrayToString(result);
-            //decrypting algorythm
         }
 
         public static bool[] FunctionS(bool[] arrRight, bool[] keyItems)
         {
-            bool test1 = function1(arrRight, keyItems);
-            bool test2 = function2(arrRight, keyItems);
-            bool test3 = function3(arrRight, keyItems);
-            bool test4 = function4(arrRight, keyItems);
-
             bool[] func = new bool[]
             {
                 function1(arrRight, keyItems),
@@ -64,12 +55,12 @@ namespace SzyfrBlokowyV5._3
         public static bool[] ArraysXor(bool[] arrLeft, bool[] arrFunc)
         {
             bool[] tempArr = new bool[4];
-            for (int i = 0; i < 4;i++) { tempArr[i] = arrLeft[i] ^ arrFunc[i]; }
+            for (int i = 0; i < 4; i++) { tempArr[i] = arrLeft[i] ^ arrFunc[i]; }
             return tempArr;
         }
         public static (bool[], bool[]) SwitchArrays(bool[] arrLeft, bool[] arrRight)
         {
-            bool[] temp; //= new bool[4];
+            bool[] temp;
             temp = arrLeft;
             arrLeft = arrRight;
             arrRight = temp;
@@ -77,12 +68,12 @@ namespace SzyfrBlokowyV5._3
         }
         public static bool[][] ReverseKeys(bool[][] keyItems)
         {
-            for (int i = 0; i < keyItems.Length/2; i++) 
+            for (int i = 0; i < keyItems.Length / 2; i++)
             {
                 bool[] temp;
                 temp = keyItems[i];
-                keyItems[i] = keyItems[7-i];
-                keyItems[7-i] = temp;
+                keyItems[i] = keyItems[7 - i];
+                keyItems[7 - i] = temp;
             }
             return keyItems;
         }
